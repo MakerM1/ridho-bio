f3a5f3c1e872e172869e035f6396f586faebdcde
MakerM1 <132217491+MakerM1@users.noreply.github.com>
2023-12-10 20:32:59 +0400

    responsive design

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+const headerLinks = document.querySelectorAll('.header-link')
+
+headerLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        for (let i = 0; i < headerLinks.length; i++) {
+            if (headerLinks[i].classList.contains('active')) {
+                headerLinks[i].classList.remove('active')
+            } else if (!headerLinks[i].classList.contains('active')) {
+                link.classList.add('active')
+            }
+        }
+    })
+})
+
+// hamburger menu
+
+const hamburgerMenu = document.querySelector('.hamburger-menu')
+const mainNavList = document.querySelector('.main-nav-list')
+const overlay = document.querySelector('.overlay')
+
+hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('active')
+    mainNavList.classList.toggle('active')
+    overlay.classList.toggle('active')
+})
+
+overlay.addEventListener('click', () => {
+    hamburgerMenu.classList.remove('active')
+    mainNavList.classList.remove('active')
+    overlay.classList.remove('active')
+})
+
+headerLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        hamburgerMenu.classList.remove('active')
+        mainNavList.classList.remove('active')
+        overlay.classList.remove('active')
+    })
+})
